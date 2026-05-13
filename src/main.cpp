b5325a41f2480463cf4de463899e0e56328d70c9
chicane <supernaturaljoe364@gmail.com>
2026-05-13 18:15:46 +0000

    implementing unordered_map for faster search

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,97 @@
 #include "scheduler.hpp"
+#include <functional>
 #include <limits>
 #include <iostream>
 #include <sstream>
 #include <string>
-#include <cstdint>
+#include <unordered_map>
 int main() {
-  Scheduler scheduler;
-
 
   while(true){
     std::string input;
     std::getline(std::cin, input);
 
-    std::string command;
-    std::string second_command;
+    Scheduler scheduler;
 
     std::istringstream iss(input);
-    iss >> command >> second_command;
+
+    std::string task_name;
+    int priority;
+    char option;
+    
+    std::unordered_map<std::string, std::function<void()>> commands = {
 
 
+      { 
+        "add task" , [&](){
+          std::cout << "Enter a task: ";
+          std::getline(std::cin, task_name);
+
+          std::cout << "Enter priority of task: ";
+          std::cin >> priority;
+
+          std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+          scheduler.addTask(task_name, priority);
+        }
+      },
+
+      {
+        "show tasks" , [&](){
+          scheduler.displayTask();
+        }
+      },
+
+      {
+        "sort tasks" , [&](){
+
+          std::cout << "Sort by (N)ame or (P)riority? :";
+          std::cin >> option;
+
+          if(option == 'N') return scheduler.sortTasksName();
+          else if(option == 'P') return scheduler.sortTasksPrio();
+        }
+      },
+
+      {
+        "load tasks" , [&](){
+          scheduler.loadTaskFile();
+        }
+      },
+
+      {
+        "save tasks" , [&](){
+          scheduler.saveTasksFile();
+        }
+      },
+
+      {
+        "remove task", [&](){
+          scheduler.removeTask(task_name);
+        }
+      },
+
+      {
+        "clear", [&](){
+          scheduler.clearFile();
+        }
+      },
+
+      {
+        "exit", [&](){
+         //i really don't know how to dea with this.
+        }
+      }
+    };
+
+    if(commands.find(input) != commands.end()){
+      //command found!
+      
+    }
+    else std::cout << "Invalid Command.\n";
+    
+  
+/*
     if (command == "add") {
     if (second_command == "task") {
       std::string task;
@@ -84,6 +157,7 @@ int main() {
         scheduler.loadTaskFile();
       }
     }
+  */
   }
       return 0;
 }
